6ab4357d83cb397c4b8035638148effe5b944b90
dodoi <dodoalt112312@gmail.com>
2022-07-26 00:43:49 -0700

    lang

--- managers/languagemanager.py
+++ managers/languagemanager.py
@@ -8,7 +8,7 @@ db = database.DataBase()
 class Language:
     def __init__(self, id):
         self.id = id
-        stmt = select(database.Guild).where(database.Guild.id == '845734294487171124')
+        stmt = select(database.Guild).where(database.Guild.id == id)
         self.lang = db.conn.execute(stmt).mappings().all()[0]["language"]
 
     async def getPhrase(self, command, phrase):
